f3c765e11a6d0141af14047773cc784364de5a81
EstebanGaMi <stevegaray1234@gmail.com>
2022-07-22 01:14:11 -0300

    final

--- js/menuhambur.js
+++ js/menuhambur.js
@@ -1,21 +1,21 @@
 
 const iconoMenu = document.querySelector('.bars__menu'),
-menu = document.querySelector('#menu');
+  menu = document.querySelector('#menu');
 
 iconoMenu.addEventListener('click', (e) => {
 
-// Alternamos estilos para el menu y body
-menu.classList.toggle('active');
-document.body.classList.toggle('opacity');
+  // Alternamos estilos para el menu y body
+  menu.classList.toggle('active');
+  document.body.classList.toggle('opacity');
 
-// Alternamos su atributo 'src' para el ícono del menú
-const rutaActual = e.target.getAttribute('src');
+  // Alternamos su atributo 'src' para el ícono del menú
+  const rutaActual = e.target.getAttribute('src');
 
-if(rutaActual == 'img/open-menu.png'){
-  e.target.setAttribute('src','img/open-menu2.png');
-}else{
-  e.target.setAttribute('src','img/open-menu.png');
-}
+  if (rutaActual == 'img/open-menu.png') {
+    e.target.setAttribute('src', 'img/open-menu2.png');
+  } else {
+    e.target.setAttribute('src', 'img/open-menu.png');
+  }
 });
 
 
@@ -25,8 +25,8 @@ var line1__bars = document.querySelector(".line1__bars-menu");
 var line2__bars = document.querySelector(".line2__bars-menu");
 var line3__bars = document.querySelector(".line3__bars-menu");
 
-function animateBars(){
-line1__bars.classList.toggle("activeline1__bars-menu");
-line2__bars.classList.toggle("activeline2__bars-menu");
-line3__bars.classList.toggle("activeline3__bars-menu");
+function animateBars() {
+  line1__bars.classList.toggle("activeline1__bars-menu");
+  line2__bars.classList.toggle("activeline2__bars-menu");
+  line3__bars.classList.toggle("activeline3__bars-menu");
 } 
